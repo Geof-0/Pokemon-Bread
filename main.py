2d144411ef1ec68e4d2a630cacf4e9555bb42445
Percy <percygandhi211@gmail.com>
2026-04-22 23:34:18 -0500

    added main loop and pgame added basic window.

--- main.py
+++ main.py
@@ -3,6 +3,31 @@
 
 import pokemon
 import pokemon_battle
+import pygame
 
-if __name__ == "main":
-    print("eiuhaeoiufhoiaef")
+pygame.init()
+
+#frames setup
+clock = pygame.time.Clock()
+fps = 75
+
+#screen setup
+screen = pygame.display.set_mode((300, 200))
+pygame.display.set_caption("Title_holder")
+
+
+#main_loop
+
+running = True
+    
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit
+
